e7824c0caa6114488a1e53c111797363f77c2d6b
edwin-cox <edwin.f.cox@gmail.com>
2023-06-03 12:10:46 +0200

    added icon

--- addons/procedural_world_map/worldmap_plugin.gd
+++ addons/procedural_world_map/worldmap_plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("ProceduralWorldMap", "ColorRect", preload("worldmap.gd"),null)
+	add_custom_type("ProceduralWorldMap", "ColorRect", preload("worldmap.gd"), preload("icon.png"))
 
 func _exit_tree():
 	remove_custom_type("ProceduralWorldMap")
